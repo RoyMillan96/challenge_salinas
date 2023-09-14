09aef29d5c904f9f3ce0397ba9da5a10645610b3
Roy Millan <millan.rodrigo201@gmail.com>
2023-09-14 15:01:20 -0600

    add endpoint to user crud endpoint

---
+++ backend/apps/users/api/user_viewsets.py
@@ -0,0 +1,43 @@
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
+from apps.users.api.serializers import UserSerializer, UserListSerializer
+from apps.users.models import User
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.filter(is_active=True).order_by('id')
+    serializer_class = UserSerializer
+
+    def get_queryset(self, pk=None):
+        if pk is None:
+            query = self.queryset.values('id', 'username', 'name', 'last_name', 'email',  'password')
+            return query
+        else:
+            return self.queryset.filter(id=pk).first()
+
+    def list(self, request):
+        user_serializer = UserListSerializer(self.get_queryset(), many=True)
+        return Response(user_serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User create successfull'}, status= status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        if self.get_queryset(pk):
+            product_serializer = self.serializer_class(self.get_queryset(pk), data=request.data)
+            if product_serializer.is_valid():
+                product_serializer.save()
+                return Response(product_serializer.data, status=status.HTTP_200_OK)
+            return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        product =  self.get_queryset(pk)
+        if product:
+            product.is_active = False
+            product.save()
+            return Response({'message': 'User deleted successfull'}, status=status.HTTP_200_OK)
+        return Response({'error': 'Do not exist this user'}, status=status.HTTP_400_BAD_REQUEST)
