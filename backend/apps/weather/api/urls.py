fe575e72f04cdcc740188b04927e432a93ca0544
Roy Millan <millan.rodrigo201@gmail.com>
2023-09-18 09:50:48 -0600

    Add enpoints user details wheater request

--- backend/apps/weather/api/urls.py
+++ backend/apps/weather/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from apps.weather.api.weather_viewsets import (
-    CurrentConditionCityGeneric, HourForecastsCityGeneric, DayForecastsCityGeneric
+    CurrentConditionCityGeneric, HourForecastsCityGeneric, DayForecastsCityGeneric,
+    UserDetailWeatherCurrents, UserDetailWeatherForecasts
 )
 
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('current-conditions/', CurrentConditionCityGeneric.as_view(), name='current_conditions'),
     path('conditions-by-hours/', HourForecastsCityGeneric.as_view(), name='conditions_by_hours'),
     path('conditions-by-days/', DayForecastsCityGeneric.as_view(), name='conditions_by_days'),
+    path('currents-user-detail/', UserDetailWeatherCurrents.as_view(), name='weatherdata-list'),
+    path('forecast-user-detail/', UserDetailWeatherForecasts.as_view(), name='weatherdata-list'),
 ]
